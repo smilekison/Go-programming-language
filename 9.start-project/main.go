45f9f311300365b930d34c9a3cf93aaf88b9c935
smilekison <smilekishan.sk@gmail.com>
2024-10-12 19:59:34 +0100

    prices struct created with logic built inside it

--- 9.start-project/main.go
+++ 9.start-project/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"example.com/price-calculator/prices"
+)
+
 func main() {
+	// var prices []float64 = []float64{10, 20, 30}
+	// var taxRates []float64 = []float64{0, 0.7, 0.1, 0.15}
+
+	// prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	// result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
+	}
 
 }
